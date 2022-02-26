f01492f39d57df3b08dc0c67bb6845b47e6c7cca
naufalnibros <mnaufalnibros@corp.bm.co.id>
2022-02-26 14:58:05 +0700

    revamp: optimize dir

---
+++ app/src/main/java/com/naufalnibros/submission_fundamental/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.naufalnibros.submission_fundamental.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.naufalnibros.submission_fundamental.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
